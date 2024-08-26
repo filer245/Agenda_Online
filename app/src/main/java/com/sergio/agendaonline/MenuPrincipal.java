4fe53999de34bdf6329849e682ae4f4bae387734
Sergio <69754375+filer245@users.noreply.github.com>
2024-08-26 16:05:52 +0200

    0.0.2: Added functionality to delete account

--- app/src/main/java/com/sergio/agendaonline/MenuPrincipal.java
+++ app/src/main/java/com/sergio/agendaonline/MenuPrincipal.java
@@ -1,5 +1,6 @@
 package com.sergio.agendaonline;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +12,14 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +30,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
 
-    Button CerrarSesion;
+    Button CerrarSesion, BorrarCuenta;
     FirebaseAuth fA;
     FirebaseUser user;
     TextView NombresPrincipal, CorreoPrincipal;
@@ -46,6 +50,7 @@ public class MenuPrincipal extends AppCompatActivity {
         ProgressBarDatos = findViewById(R.id.ProgressBarDatos);
 
         Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
+        BorrarCuenta = findViewById(R.id.BorrarCuenta);
         CerrarSesion = findViewById(R.id.CerrarSesion);
         fA = FirebaseAuth.getInstance();
         user = fA.getCurrentUser();
@@ -54,6 +59,46 @@ public class MenuPrincipal extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 SalirAplicacion();
+            }
+        });
+
+        BorrarCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alerta = new AlertDialog.Builder(MenuPrincipal.this);
+                alerta.setMessage("Esto borrará su cuenta para siempre.")
+                        .setPositiveButton("Borrar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                BorrarUser();
+                            }
+                        }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog titAlerta = alerta.create();
+                titAlerta.setTitle("Borrar cuenta");
+                titAlerta.show();
+            }
+        });
+    }
+
+    private void BorrarUser() {
+        fA.getCurrentUser().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Usuarios.child(user.getUid()).removeValue();
+                Toast.makeText(MenuPrincipal.this, "Cuenta Borrada", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println(e.getMessage());
+                Toast.makeText(MenuPrincipal.this, "ERROR: "+e.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -84,6 +129,8 @@ public class MenuPrincipal extends AppCompatActivity {
                     //Mostramos Datos
                     NombresPrincipal.setVisibility(View.VISIBLE);
                     CorreoPrincipal.setVisibility(View.VISIBLE);
+                    BorrarCuenta.setVisibility(View.VISIBLE);
+                    CerrarSesion.setVisibility(View.VISIBLE);
                     //Traemos datos
                     String nombres = ""+snapshot.child("nombres").getValue();
                     String correo = ""+snapshot.child("correo").getValue();
